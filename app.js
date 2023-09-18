1f41eccdacb6b43cb896676f3ed625c3f360d822
shoaib <shoaibhossain24@gmail.com>
2023-09-19 04:46:32 +0600

    add project structure for ejs project

--- app.js
+++ app.js
@@ -5,15 +5,23 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-
-
-
-
 const app = express();
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+"/public"));
 
+
+
+
+// get
+
+app.get('/', function(req, res){
+
+    res.render('homepage', {title: "Home"});
+
+
+});
 
 
 
