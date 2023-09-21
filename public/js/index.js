4b59ec143a39ee76f68393d8c8f278b67a1df0cc
shoaib <shoaibhossain24@gmail.com>
2023-09-21 09:04:10 +0600

    fix navbar highlight issue

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
 // pre loader start
 function loader() {
   document.querySelector(".loader-container").classList.add("fade-out");
@@ -20,17 +19,46 @@ window.onload = fadeOut;
 // pre loader end
 
 //scroll funciton
-document.querySelectorAll('a[href*="#"]').forEach(function(anchor) {
-  anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      var targetId = this.getAttribute('href').substring(1);
-      var targetElement = document.getElementById(targetId);
+document.querySelectorAll('a[href*="#"]').forEach(function (anchor) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    var targetId = this.getAttribute("href").substring(1);
+    var targetElement = document.getElementById(targetId);
 
-      if (targetElement) {
-          window.scrollTo({
-              top: targetElement.getBoundingClientRect().top + window.scrollY,
-              behavior: 'smooth'
-          });
-      }
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.getBoundingClientRect().top + window.scrollY,
+        behavior: "smooth",
+      });
+    }
   });
 });
+
+//highlight navbar
+
+function highlightNavItemBasedOnPage() {
+  // Get the current page path (e.g., /education, /experience, etc.)
+  var currentPagePath = window.location.pathname;
+
+  // Select the navigation items
+  var navItems = document.querySelectorAll('header .navbar ul li a');
+
+  // Loop through the navigation items
+  navItems.forEach(function(navItem) {
+      // Remove the "active" class from all navigation items
+      navItem.classList.remove('active');
+
+      // Get the URL specified in the 'href' attribute of the navigation item
+      var navItemUrl = navItem.getAttribute('href');
+
+      // Check if the current page path matches the navigation item's URL
+      if (currentPagePath === navItemUrl) {
+          // Add a class to highlight the navigation item
+          navItem.classList.add('active');
+      }
+  });
+}
+
+// Call the function when the page loads
+window.addEventListener('load', highlightNavItemBasedOnPage);
+// Call the function when the page loads
